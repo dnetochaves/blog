fc2f9ae10d89f2194a3fa3f8eeb8e90e6a56099c
dnetochaves <dnetochaves@gmail.com>
2020-12-26 16:50:53 -0300

    Finalizaçao 1 parte

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostIndex.as_view(), name='index'),
-    path('categoria/', views.PostCategoria.as_view(), name='index'),
-    path('busca/', views.PostBusca.as_view(), name='index'),
-    path('posts_detalhes/<int:pk>/', views.PostDetalhes.as_view(), name='posts_detalhes'),
+    path('categoria/<str:categoria>', views.PostCategoria.as_view(), name='post_categoria'),
+    path('busca/', views.PostBusca.as_view(), name='post_busca'),
+    path('post/<int:pk>', views.PostDetalhes.as_view(), name='post_detalhes'),
 ] 
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView  
-from django.views.generic.edit import UpdateView 
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
 from . models import Post
+from django.db.models import Q, Count, Case, When
+from comentarios.models import Comentarios
+from comentarios.forms import FormComentario
+from django.contrib import messages
+
 
 class PostIndex(ListView):
     model = Post
@@ -9,11 +14,77 @@ class PostIndex(ListView):
     paginate_by = 3
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.select_related('categoria_post')
+        qs = qs.order_by('-id').filter(publicacao_post=True)
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentarios__publicado_comentario=True, then=1)
+                )
+            )
+        )
+
+        return qs
+
+
 class PostBusca(PostIndex):
-    pass
+    template_name = 'posts/post_busca.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        termo = self.request.GET.get('termo')
+
+        if not termo:
+            return qs
+
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) |
+            Q(autor_post__first_name__iexact=termo) |
+            Q(conteudo_post__icontains=termo) |
+            Q(exerto_post__icontains=termo) |
+            Q(categoria_post__nome_cat__iexact=termo)
+        )
+        return qs
+
 
 class PostCategoria(PostIndex):
-    pass
+    template_name = 'posts/post_categoria.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        categoria = (self.kwargs.get('categoria', None))
+
+        if not categoria:
+            return qs
+
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+        return qs
+
 
 class PostDetalhes(UpdateView):
-    pass
+    template_name = 'posts/posts_detalhes.html'
+    model = Post
+    form_class = FormComentario
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.get_object()
+        comentarios = Comentarios.objects.filter(
+            publicado_comentario=True, post_comentario=post.id)
+        context['comentarios'] = comentarios
+        return context
+
+    def form_valid(self, form):
+        post = self.get_object()
+        comentario = Comentarios(**form.cleaned_data)
+        comentario.post_comentario = post
+
+        if self.request.user.is_authenticated:
+            comentario.usuario_comentario = self.request.user
+
+        comentario.save()
+        messages.success(self.request, 'Comentário enviado com sucesso.')
+        return redirect('post_detalhes', pk=post.id)
